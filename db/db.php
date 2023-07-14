5c662654d3652fa5c180ee7714b3722167fe089c
Ericson-Manalo <ericmana98@gmail.com>
2023-07-14 16:46:53 +0200

    called my file by using include_once

---
+++ db/db.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once __DIR__ '/../classes/Product.php';
+include_once __DIR__ '/../classes/Food.php';
+include_once __DIR__ '/../classes/Toy.php';
+include_once __DIR__ '/../classes/Kennel.php';
+
+
+
+
+
+
+?>
