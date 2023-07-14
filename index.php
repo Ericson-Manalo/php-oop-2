65b8467c48c921ad0a9e6f616f39773ae92ade8b
Ericson-Manalo <ericmana98@gmail.com>
2023-07-14 23:24:19 +0200

    trying to create first cards

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+    include_once __DIR__ . '/db/db.php';
+    include_once __DIR__ . '/classes/Food.php';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Animal's shop</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Booleapet's Shop</h1>
+        <div class="row">
+            <div class="col-12">
+                Shop:
+            </div>
+            <?php foreach ($foods as $food) { ?>
+                <div class="card col-3 p-0 m-3" >
+                    <div class="card-header">
+                        <?php echo $food->name; ?>
+                    </div>
+                    <img src="<?php echo $food->image ?>"  class="card-img-top rounded-0" alt="<?php echo $food->name; ?>">
+                    <div class="card-body p-3">
+                        <h5 class="card-title">
+                            <?php echo $food->name; ?>
+                        </h5>
+                    
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</body>
+</html>
