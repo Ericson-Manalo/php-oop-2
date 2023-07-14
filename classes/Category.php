eac5b3340464d5772a7c8307ea2572b4fd93eb45
Ericson-Manalo <ericmana98@gmail.com>
2023-07-14 16:06:57 +0200

    fill the class category by giving name and age properties

---
+++ classes/Category.php
@@ -0,0 +1,24 @@
+<?php 
+
+<?php 
+
+class Category {
+    public $name;
+    public $age;
+
+
+    function __construct (String $name, Int $age){
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+}
+
+?>
+
+
+
+
+
+
+?>
